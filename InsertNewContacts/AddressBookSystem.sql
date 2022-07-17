257f44057634f6950aac6a4107107669b4574f03
AtulSikarwal <atulsikarwal5.com>
2022-07-17 21:25:39 +0530

    The code for Insert New Contacts

---
+++ InsertNewContacts/AddressBookSystem.sql
@@ -0,0 +1,20 @@
+create database AddressBookService;
+
+create table AddressBook(
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(100),
+City varchar(100),
+State varchar (100),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(100)
+);
+
+insert into AddressBook(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+values('Atul','Sikarwal','NEB','Alwar','Rajasthan',301001,9983922916,'atulsikarwal5@gmail.com'),
+('Karan','Kumar','vimlacolony','jaipur','Rajasthan',302020,7076855485,'ks@gmail.com'),
+('Akash','Rajput','60foot','Agra','UP',234567,7976065986,'akash@gmail.com'),
+('chunnu','panwar','churimarket','ajmer','Rajasthan',342675,4456546734,'chunnu@gmail.com');
+
+select * from AddressBook;

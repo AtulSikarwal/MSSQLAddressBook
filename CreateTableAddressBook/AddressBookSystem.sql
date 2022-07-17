f4b31090befdc1e0db7555ae4222441be1f4ca8c
AtulSikarwal <atulsikarwal5.com>
2022-07-17 21:16:59 +0530

    The code for Create Table AddressBook

---
+++ CreateTableAddressBook/AddressBookSystem.sql
@@ -0,0 +1,13 @@
+create database AddressBookService;
+
+create table AddressBook(
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(100),
+City varchar(100),
+State varchar (100),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(100)
+);
+
